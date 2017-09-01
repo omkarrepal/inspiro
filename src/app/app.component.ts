6dfd1031e04f3a758530e777d24a029af1cf31bd
omkar repal <omyrepal@gmail.com>
2017-09-01 09:33:35 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase/app';
+import {FilterPipe} from './filter.pipe'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'app';
+  user: Observable<firebase.User>;
+  items: FirebaseListObservable<any[]>;
+  msgVal: string = '';
+  constructor(public afAuth: AngularFireAuth, public af: AngularFireDatabase) {
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 100
+      }
+    });
+
+    this.user = this.afAuth.authState;
+    
+
+  }
+  login() {
+    this.afAuth.auth.signInAnonymously();
 }
+
+logout() {
+    this.afAuth.auth.signOut();
+}
+
+Send(desc: string,ref:string) {
+  
+  
+  if(this.msgVal!='')
+    {
+    this.items.push({ message: desc});
+    
+    this.msgVal = '';
+    
+    document.body.scrollTop = document.body.scrollHeight;
+    }
+     else{
+      window.alert("Are you running out of Inspiration?")
+    }
+
+}
+  
+
+
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { AppComponent } from './app.component';
+import { FilterPipe } from './filter.pipe';
+
+export const firebaseConfig ={
+    apiKey: "AIzaSyBDRSNa5VNn-R4GTPHayck9r-J6TN661dA",
+    authDomain: "inspiro-7595.firebaseapp.com",
+    databaseURL: "https://inspiro-7595.firebaseio.com",
+    projectId: "inspiro-7595",
+    storageBucket: "inspiro-7595.appspot.com",
+    messagingSenderId: "633685403834"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
